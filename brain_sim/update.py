9d85cdce8503d68b3462c6d10e9475467b48ca7e
Alexander Birtles <aloxemail@gmail.com>
2025-09-30 19:24:46 +1000

    feat: implement backwards pass for error-driven synapse connection weights update between layer updates in predictive coding loop

--- brain_sim/update.py
+++ brain_sim/update.py
@@ -1,13 +1,44 @@
 from .config import SIZE
 
 def step_predictive(brain, alpha, size = SIZE):
+    alpha = alpha
     sensors     = brain.sns
     predictions = brain.prd
     connections = brain.connections
 
+    sens_hat = [[0.0 for _ in range(size)] for _ in range (size)]
+
     for y, row in enumerate(predictions):
         for x, cell in enumerate(row):
-            weights = connections[y][x]
+            for dy_i in range(3):
+                for dx_i in range(3):
+                    dy = dy_i - 1
+                    dx = dx_i - 1
+                    ny, nx = y + dy, x + dx
+                    if 0 <= ny < size and 0 <= nx < size:
+                        sens_hat[ny][nx] += predictions[y][x] * connections[y][x][dy_i][dx_i]
+
+    errors = [[0.0 for _ in range(size)] for _ in range(size)]
+
+    for y in range(size):
+        for x in range(size):
+            errors[y][x] = sensors[y][z] - sens_hat[y][z]
+
+    for y, row in enumerate(predictions):
+        for x, cell in enumerate(row):
+            for dy_i in range(3):
+                for dx_i in range(3):
+                    dy = dy_i - 1
+                    dx = dx_i - 1
+                    ny, nx = y + dy, x + dx
+                    if 0 <= ny < size and 0 <= nx < size:
+                        error    = errors[ny][nx]
+                        weight = connections[y][x][dy_i][dx_i]
+                        connections[y][x][dy_i][dx_i] = weight + alpha * error * predictions[x][y]
+
+
+    for y, row in enumerate(predictions):
+        for x, cell in enumerate(row):
             sum     = 0
             for dy_i in range(3):
                 for dx_i in range(3):
